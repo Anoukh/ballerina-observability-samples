86bbcc52443baa22629e4de32dda91a4838d3440
Anoukh <anoukh.ashley@gmail.com>
2018-05-30 15:35:11 +0530

    Add SQL script

---
+++ resources/testdb.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 30, 2018 at 03:34 PM
+-- Server version: 5.7.22-0ubuntu0.16.04.1
+-- PHP Version: 7.0.30-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ORDERS`
+--
+
+CREATE TABLE `ORDERS` (
+  `orderId` int(11) DEFAULT '1',
+  `productId` int(11) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ORDERS`
+--
+
+INSERT INTO `ORDERS` (`orderId`, `productId`) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(1, 4),
+(2, 2),
+(2, 5),
+(2, 3),
+(3, 4),
+(3, 5),
+(3, 6),
+(4, 1),
+(4, 5),
+(4, 4),
+(4, 5),
+(4, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PRODUCT`
+--
+
+CREATE TABLE `PRODUCT` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` float NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `PRODUCT`
+--
+
+INSERT INTO `PRODUCT` (`id`, `name`, `price`) VALUES
+(1, 'ASUS Vivo Stick PC - TS10', 25900),
+(2, 'DCduino Uno', 675),
+(3, 'Raspberry Pi 3 Model B', 8000),
+(4, 'Arduino Starter Kit', 3500),
+(5, 'HP 15-BS521TU', 61000),
+(6, 'Xiaomi Mi Power', 3450);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `PRODUCTS`
+--
+
+CREATE TABLE `PRODUCTS` (
+  `ID` int(11) NOT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  `COUNT` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `PRODUCTS`
+--
+
+INSERT INTO `PRODUCTS` (`ID`, `NAME`, `COUNT`) VALUES
+(1, 'APIM', 10),
+(2, 'IS', 20);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `PRODUCT`
+--
+ALTER TABLE `PRODUCT`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `PRODUCTS`
+--
+ALTER TABLE `PRODUCTS`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `PRODUCT`
+--
+ALTER TABLE `PRODUCT`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `PRODUCTS`
+--
+ALTER TABLE `PRODUCTS`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
